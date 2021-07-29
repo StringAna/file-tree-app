b6b7cb26df467c98983f471f7d25eab4873d797e
StringAna <tewaryantara21@gmail.com>
2021-07-29 09:25:03 +0530

    Adding Folders inside Desktop

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
       <Folder name="Desktop">
+        <Folder name="Music">
+          <File name="song1.mp4" />
+          <File name="song2.mp4" />
+        </Folder>
         <File name="Dogs.jpeg" />
         <File name="Cats.jpeg" />
       </Folder>
