39ee64cc82d0d1fcb542036c73f8eb70a4350995
StringAna <tewaryantara21@gmail.com>
2021-07-29 09:03:38 +0530

    Dynamically passing data from App to Folder component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <Folder />
-      <Folder />
-      <Folder />
+      <Folder name="Desktop" />
+      <Folder name="Applications" />
     </div>
   );
 }
 
-const Folder = () => {
-  return <h4>Folder</h4>;
+const Folder = (props) => {
+  console.log(props);
+  return <h4>{props.name}</h4>;
 };
 
 export default App;
