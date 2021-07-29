abe15cd279801fe02855403e764c1069af91f60a
StringAna <tewaryantara21@gmail.com>
2021-07-29 10:40:44 +0530

    Render diff icons depending on type of file

--- src/App.js
+++ src/App.js
@@ -42,6 +42,21 @@ const Folder = (props) => {
 
 //An individual entity
 const File = (props) => {
-  return <div>{props.name}</div>;
+  const { name } = props;
+  //split on . , take the second element
+  const fileExtension = name.split(".")[1];
+
+  //object to map the file extenstion to the type of icon
+  const fileIcons = {
+    mp4: "headphones",
+    jpeg: "file image",
+    png: "file image outline",
+  };
+  return (
+    <div>
+      <i className={`${fileIcons[fileExtension]} icon`}></i>
+      {name}
+    </div>
+  );
 };
 export default App;
