b8ba1ce21815dfb6c364bb1afe8ad0a79e56e107
StringAna <tewaryantara21@gmail.com>
2021-07-29 08:56:33 +0530

    Created Folder Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import React from "react";
+
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Folder />
+      <Folder />
+      <Folder />
+    </div>
+  );
 }
+
+const Folder = () => {
+  return <h4>Folder</h4>;
+};
 
 export default App;
