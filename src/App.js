b5930625a85132189696eb39876aeee4b2f9c9a5
StringAna <tewaryantara21@gmail.com>
2021-07-29 09:06:43 +0530

    Created File component.Applied similar func as in Folder cpt.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ function App() {
     <div className="App">
       <Folder name="Desktop" />
       <Folder name="Applications" />
+      <File name="Dogs.jpeg" />
     </div>
   );
 }
 
+//An entity containing more things
 const Folder = (props) => {
   console.log(props);
   return <h4>{props.name}</h4>;
 };
 
+//An individual entity
+const File = (props) => {
+  return <h5>{props.name}</h5>;
+};
 export default App;
